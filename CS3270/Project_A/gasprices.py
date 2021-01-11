b841de449cc8bb662704380ec4999ddc425b1db4
stevenshow <steven.schoebinger@gmail.com>
2021-01-10 23:46:12 -0700

    Finished Min/Max functions

--- CS3270/Project_A/gasprices.py
+++ CS3270/Project_A/gasprices.py
@@ -24,7 +24,9 @@ def process_file():
         if year_counter == curr_year:               #grabs month from date string
             month = month_data[0][:2]
             if month.startswith('0'):
-                month = month[1:]
+                month = int(month[1:])
+            elif month.startswith('1'):
+                month = int(month[:2])
             if month not in year_dict.keys():
                 year_dict[month] = [month_data[1]]
             else:    
@@ -37,19 +39,34 @@ def process_file():
 
 def export_year_info(year_dict, year):
     with open('/home/steven/Documents/Python_code/CS3270/Project_A/gas_report.txt', 'a+') as out:
-        min_price = get_min(year_dict)
-        
+        min_price = float(get_min(year_dict))
+        max_price = float(get_max(year_dict))
+        print(f'Low: {round(min_price, 3)}, Avg:___, High: {round(max_price, 3)}')
 
 def get_min(year_dict):
-    min_key = min(year_dict, key=year_dict.get)
-    temp = year_dict[min_key]
-    return min(temp)
+    #min_key = min(year_dict, key=int)
+    #temp = year_dict[min_key]
+    #return min(temp)
+    temp_min = float(year_dict[1][0])
+    for lists in year_dict:
+        for num in year_dict[lists]:
+            if float(temp_min) > float(num):
+                temp_min = num
+                break
+    return temp_min
     
 
 def get_max(year_dict):
-    max_key = max(year_dict, key=year_dict.get)
-    temp = year_dict[max_key]
-    return max(temp)
+    #max_key = max(year_dict, key=int)
+    #temp = year_dict[max_key]
+    #return max(temp)
+    temp_max = float(year_dict[1][0])  
+    for lists in year_dict:
+        for num in year_dict[lists]:
+            if float(temp_max) < float(num):
+                temp_max = num
+    return temp_max
+
  
 def get_average_year(year_dict):
     pass
@@ -57,6 +74,6 @@ def get_average_year(year_dict):
 def get_average_month():
     pass
 
-process_file()
-test = {'1': [10,30,50], '2': [50,20,5], '3': [0,50,100]}
-#export_year_info(test, 1994)
+#process_file()
+test = {1: ['10.123', '30.34', '200.3423', '49.6', '4.92034'], 2: ['1.3', '48.234', '20', '5'], 3: ['12', '49.12342', '100.23']}
+export_year_info(test, 1994)

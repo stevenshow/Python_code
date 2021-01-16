f32fc81e948e9f5cb29cc96030d098225bcb7dba
stevenshow <steven.schoebinger@gmail.com>
2021-01-15 22:01:34 -0700

    created wc.py file and listed tasks

---
+++ CS3270/Exercise_2/wc.py
@@ -0,0 +1,7 @@
+'''Reads to file provided and counts all the letters and apostrophes,
+then outputs to a file the count of each letter (case does not matter)'''
+# [] Read file
+# [] Store file data and process file
+# [] Convert all letters to lower case and count
+# [] Output count and words in a nice column format
+

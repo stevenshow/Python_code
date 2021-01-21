e30eb48249624e378859483c023ce481be768d07
stevenshow <steven.schoebinger@gmail.com>
2021-01-21 12:50:34 -0700

    .

--- CS3270/Exercise_2/wc.py
+++ CS3270/Exercise_2/wc.py
@@ -1,15 +1,17 @@
+import re
 '''Reads to file provided and counts all the letters and apostrophes,
 then outputs to a file the count of each letter (case does not matter)'''
-import re
 # [] Read file
 # [] Store file data and process file
 # [] Count only words that have letters and apostrophes (everything lowercase)
 # [] Output count and words in a nice column format
-
 def count_words():
+    word_pattern = '[A-Za-z\']+'
+    print(word_pattern)
     with open("/home/steven/Documents/Python_code/CS3270/Exercise_2/Strings.txt") as f: 
-        for line in f:
-            for word in line.split():
-                print(re.sub(r'\w*\d\w*', '', word).strip())
+        file = f.read()
+        my_list = re.findall(word_pattern, file)
+        print(my_list)
+        
 
 count_words()

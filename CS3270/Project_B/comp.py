5583568d63a25ecac24b78d4e9a403295f9594d0
stevenshow <steven.schoebinger@gmail.com>
2021-02-02 16:08:16 -0700

    Added reading from file directly to get named tuples

--- CS3270/Project_B/comp.py
+++ CS3270/Project_B/comp.py
@@ -2,6 +2,7 @@
 or operate in different cities by using dict comprehension
 Created by: Steven Schoebinger 1/28/2021'''
 import parts
+from collections import namedtuple
 # pylint: disable=invalid-name, undefined-loop-variable
 # [X] Get names of all suppliers that supply bolts.
 # [X] Get names of all suppliers that supply blueparts.
@@ -10,37 +11,71 @@ import parts
 # [X] Get pairs of names of all suppliers that are located in the same city.
 # [X] Print all suppliers out by city
 
+suppliers = set()
+parts = set()
+projects = set()
+spj = set()
+
+def read_files():
+    Supplier = namedtuple('Supplier',['sno', 'sname', 'status', 'city'])
+    Part = namedtuple('Part',['pno', 'pname', 'color', 'weight', 'city'])
+    Project = namedtuple('Project',['jno', 'jname', 'city'])
+    SPJ = namedtuple('SPJ',['sno', 'pno', 'jno', 'qty'])
+    
+    with open('/home/steven/Documents/Python_code/CS3270/Project_B/suppliers.txt', 'r') as f:
+        temp= f.read().splitlines() #take out newline char
+        for line in temp:
+            line = line.split(',')
+            suppliers.add(Supplier(line[0], line[1], line[2], line[3]))    
+    with open('/home/steven/Documents/Python_code/CS3270/Project_B/parts.txt', 'r') as f:
+        temp= f.read().splitlines() #take out newline char
+        for line in temp:
+            line = line.split(',')
+            parts.add(Part(line[0], line[1], line[2], line[3], line[4]))
+    with open('/home/steven/Documents/Python_code/CS3270/Project_B/projects.txt', 'r') as f:
+        temp= f.read().splitlines() #take out newline char
+        for line in temp:
+            line = line.split(',')
+            projects.add(Project(line[0], line[1], line[2]))
+    with open('/home/steven/Documents/Python_code/CS3270/Project_B/spj.txt', 'r') as f:
+        temp= f.read().splitlines() #take out newline char
+        for line in temp:
+            line = line.split(',')
+            spj.add(SPJ(line[0], line[1], line[2], line[3]))
+
+read_files()
+
 def bolt_suppliers():
     '''Populates a set with the names of the suppliers that supply
     botls.'''
-    bolt_pno = {p.pno for p in parts.parts if 'Bolt' in p}
-    bolt_sno = {s.sno for s in parts.spj if s.pno in bolt_pno}
-    bolt_supplier = {s.sname for s in parts.suppliers if s.sno in bolt_sno}
+    bolt_pno = {p.pno for p in parts if 'Bolt' in p}
+    bolt_sno = {s.sno for s in spj if s.pno in bolt_pno}
+    bolt_supplier = {s.sname for s in suppliers if s.sno in bolt_sno}
     print(bolt_supplier)
 
 def blue_part_suppliers():
     '''Populates a set with the names of the suppliers that supply
     blue parts.'''
-    blue_pno = {p.pno for p in parts.parts if p.color == 'Blue'}
-    blue_sno = {s.sno for s in parts.spj if s.pno in blue_pno}
-    blue_supplier = {s.sname for s in parts.suppliers if s.sno in blue_sno}
+    blue_pno = {p.pno for p in parts if p.color == 'Blue'}
+    blue_sno = {s.sno for s in spj if s.pno in blue_pno}
+    blue_supplier = {s.sname for s in suppliers if s.sno in blue_sno}
     print(blue_supplier)
 
 def not_Athens():
     '''Populates a set with the names of the suppliers that are not
     used in the Athens projects.'''
-    athens_proj = {p.jno for p in parts.projects if p.city == 'Athens'}
-    noAthens_spj = {s.sno for s in parts.spj if s.jno in athens_proj}
-    noAthens_supplier = {s.sname for s in parts.suppliers if s.sno not in noAthens_spj}
+    athens_proj = {p.jno for p in projects if p.city == 'Athens'}
+    noAthens_spj = {s.sno for s in spj if s.jno in athens_proj}
+    noAthens_supplier = {s.sname for s in suppliers if s.sno not in noAthens_spj}
     print(noAthens_supplier)
 
 def not_Oslo():
     '''Populates a set with the names and colors of the parts that
     are not used in the city of Oslo.'''
-    oslo_jno = {j.jno for j in parts.projects if j.city == 'Oslo'}
-    oslo_pno = {p.pno for p in parts.spj if p.jno in oslo_jno}
-    not_oslo_parts = {p.pno for p in parts.spj if p.pno not in oslo_pno}
-    parts_set = {(p.pname, p.color) for p in parts.parts if p.pno in not_oslo_parts}
+    oslo_jno = {j.jno for j in projects if j.city == 'Oslo'}
+    oslo_pno = {p.pno for p in spj if p.jno in oslo_jno}
+    not_oslo_parts = {p.pno for p in spj if p.pno not in oslo_pno}
+    parts_set = {(p.pname, p.color) for p in parts if p.pno in not_oslo_parts}
     print(parts_set)
 
 def same_city():
@@ -50,15 +85,15 @@ def same_city():
     then making sure they are in the same city, and then making sure that
     there is not another duplicate in the set already by making sure that
     set is always in that configuration.'''
-    city_supp = {(s1.sname, s2.sname) for s1 in parts.suppliers
-        for s2 in parts.suppliers if s1 != s2 and s1.city == s2.city and s1 < s2}
+    city_supp = {(s1.sname, s2.sname) for s1 in suppliers
+        for s2 in suppliers if s1 != s2 and s1.city == s2.city and s1 < s2}
     print(city_supp)
 
 def sup_by_city():
     '''Populates a dictionary of sets with the key being the city and the set
     within the dictionary being the suppliers who supply in that city.'''
-    city_suppliers = {s2.city:{s1.sname for s1 in parts.suppliers if s1.city == s2.city}
-        for s2 in parts.suppliers}
+    city_suppliers = {s2.city:{s1.sname for s1 in suppliers if s1.city == s2.city}
+        for s2 in suppliers}
     print(city_suppliers)
 
 bolt_suppliers()

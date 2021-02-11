cd75f6aca8897ee2c0d4eb9d2eb72ef2a30f7d82
stevenshow <steven.schoebinger@gmail.com>
2021-02-11 12:51:23 -0700

    Got pylint up to 10/10

--- CS3270/Project_C/word_game.py
+++ CS3270/Project_C/word_game.py
@@ -1,9 +1,16 @@
+'''Chooses a word based on user input and grabs all the substrings of that word
+based on user input.  The user is then prompted to enter guesses and if the user
+is correct the dashed word will be replaced with the users correct guess
+if the user is wrong, the program will shuffle the word and let the user
+guess again.  If the user is done guessing and wants to exit, they can enter
+'q' and all the words will be revealed and the program will exit'''
 from itertools import combinations
 import random
-import nltk
-import string
 import re
 import copy
+import sys
+import nltk
+# pylint: disable=invalid-name, undefined-loop-variable
 # [X] Parse text file and create list of words
 # [X] Create dictionary with length and starting letter as key
 # [X] Get user input to choose what word lengths to choose
@@ -16,24 +23,26 @@ low = int(low_high[0])
 high = int(low_high[1:])
 
 def get_user():
-    #print(sorted_words)
+    '''Gets the user input and gives feedback on correct word or not
+    if the user is correct it will replcae the dashes with the correct word
+    if the user is incorrect it will say try again and shuffle the word'''
     total = (sum([len(dotted_words[x]) for x in dotted_words if isinstance(dotted_words[x], list)]))
     correct = 0
-    while(True):
+    while True:
         last_shuffle = shuffle()
-        if(shuffle() != last_shuffle):
-            print('\n' + 
+        if shuffle() != last_shuffle:
+            print('\n' +
             shuffle() + ':\n')
         else:
             break
         for i in dotted_words:
             print(dotted_words[i])
         guess = input('\nEnter a guess: ')
-        if(guess == 'q'):
+        if guess == 'q':
             print('\n')
             for i in sorted_words:
                 print(sorted_words[i])
-            quit()
+            sys.exit()
         if len(guess) in sorted_words:
             if guess in sorted_words[len(guess)]:
                 print('Correct!')
@@ -44,37 +53,31 @@ def get_user():
                 print('Try again!')
         else:
             print('Try again')
-        if(correct == total):
+        if correct == total:
             print('\n')
             for i in dotted_words:
                 print(dotted_words[i])
-            quit()
+            sys.exit()
 
 def shuffle():
     '''Shuffles the word each time that the user guesses a word'''
     word_list = list(word)
     random.shuffle(word_list)
     shuffled_word = ''.join(word_list)
-    return(shuffled_word)
+    return shuffled_word
 
 def choose_word():
     '''Takes the high input and also gets a random letter to use as a tuple to search
     in the dictionary tuple keys'''
-    word = ''
-    letter = random.randint(97, 122)
+    chosen_word = ''
     word_list = []
     dict_keys = list(word_dict.keys())
     for x in dict_keys:
         if x[1] == high:
             word_list.append(x)
-    print(word_list)
     choice = random.choice(word_list)
-    word = random.choice(word_dict[choice])
-    #if (chr(letter), high) in word_dict.keys():
-    #    word = word_dict[(chr(letter), high)][0]
-    #else:
-    #    letter = random.randint(97, 122)
-    return word
+    chosen_word = random.choice(word_dict[choice])
+    return chosen_word
 
 def dict_tuple():
     '''Creates a dictionary with (starting letter of word, length of word) as the key and
@@ -82,44 +85,43 @@ def dict_tuple():
     EX. {('a', 3): ['abs', 'art']'''
     path = '/home/steven/Documents/Python_code/CS3270/Project_C/'
     with open(path + 'words.txt', 'r') as f:
-        word_dict = {}
+        word_tup = {}
         file = f.read()
         words = nltk.word_tokenize(file)
-        for word in words:
-            if (word[0].lower(), len(word)) in word_dict:
-                word_dict[(word[0].lower(), len(word))].append(word.lower())
+        for ele in words:
+            if (ele[0].lower(), len(ele)) in word_tup:
+                word_tup[(ele[0].lower(), len(ele))].append(ele.lower())
             else:
-                word_dict[(word[0].lower(), len(word))] = [word.lower()]
-        return word_dict
+                word_tup[(ele[0].lower(), len(ele))] = [ele.lower()]
+        return word_tup
 
 def process_dict(guess_list):
     '''Scrambles chosen word and creates subset of all words of length
     low->high+1'''
-    word_lengths = set()
+    lengths = set()
     guess_list = set()
     for i in range(low, high+1):
-        #words are not fully scrambling for every combination
         letter_tuples = list(combinations(word, i))
-        my_words = [''.join(tups) for tups in letter_tuples]
-        process_dict_helper(my_words, guess_list, word_lengths)
-    return word_lengths, guess_list
+        words = [''.join(tups) for tups in letter_tuples]
+        process_dict_helper(words, guess_list, lengths)
+    return lengths, guess_list
 
-def process_dict_helper(my_words, guess_list, word_lengths):
+def process_dict_helper(words, guess_list, lengths):
     '''Helper function for process_dict() to place words into list'''
-    for word in my_words:
-        if (word[0], len(word)) in word_dict:
-            if word in word_dict[(word[0], len(word))]:   
-                guess_list.add(word)
-                word_lengths.add(len(word))
+    for ele in words:
+        if (ele[0], len(ele)) in word_dict:
+            if ele in word_dict[(ele[0], len(ele))]:
+                guess_list.add(ele)
+                lengths.add(len(ele))
 
-def dotted_list(my_words, word_lengths):
+def dotted_list(words, lengths):
     '''Creates two dictionaries.  One is the dictinoary with key(len of word):
     value(words in alphabetical order.  The other dictionary is a deep copy
     of the first dictionary with all the words replaced with '-' for guessing.'''
-    sorted_words = sorted(list(my_words))
-    sorted_dict = {i: [] for i in word_lengths}
-    for word in sorted_words:
-        sorted_dict[len(word)].append(word)
+    sort_words = sorted(list(words))
+    sorted_dict = {i: [] for i in lengths}
+    for ele in sort_words:
+        sorted_dict[len(ele)].append(ele)
     dotted_dict = copy.deepcopy(sorted_dict)
     for i in dotted_dict.keys():
         for j in range(len(dotted_dict[i])):
@@ -133,27 +135,3 @@ word_lengths, my_words = process_dict(my_words)
 sorted_words, dotted_words = dotted_list(my_words, word_lengths)
 get_user()
 choose_word()
-
-#def dict_creator():
-#    path = '/home/steven/Documents/Python_code/CS3270/Project_C/'
-#    with open(path + 'words.txt', 'r') as f:
-#        my_dict = {}
-#        file = f.read()
-#        words = nltk.word_tokenize(file)
-#        #print(words)
-#        for word in words:
-#            if word[0].lower() in my_dict:    
-#                my_dict[word[0].lower()].append(word.lower())
-#            else:
-#                my_dict[word[0].lower()] = [word.lower()]    
-#        print(my_dict)
-#
-#    word = 'burner'
-#    letter_tuples = list(combinations(word, 3))
-#    my_words = [''.join(tups) for tups in letter_tuples]
-#    print(my_words)
-#    for word in my_words:
-#        if word[0] in my_dict:
-#            if word in my_dict[word[0]]:
-#                print(word)
-#    print(my_words)

32dcaaecbb8e1fc7a229d82fb90487a2a9b56e97
stevenshow <steven.schoebinger@gmail.com>
2021-02-16 22:08:55 -0700

    Moving forward

--- CS3270/Exercise_3/dec.py
+++ CS3270/Exercise_3/dec.py
@@ -1,34 +1,34 @@
+import os
+path = os.path.dirname(__file__)
+
 def track(f):
     def wrapper(*args1, **args2):
-        wrapper.count += 1        
+        #print(wrapper.count)      
+        wrapper.count += 1
         return f(*args1, **args2)    
     wrapper.count = 0    
     return wrapper
 
-def trace(func):
-    def wrapper(*args1, **args2):
-        print(func.__name__,'with',args1,args2)        
-        return func(*args1, **args2)    
-    return wrapper
-
-def storeargs(arglist):
+def log(arglist):
     def real(f):
         def wrapper(*args,**kwargs):
-            key = str(args)+str(kwargs)
-            if key in arglist:
-                print(key + 'found in cache')
-            else:   
-                arglist.add(key)
+            with open(path + '/log.txt', 'a+') as logfile:
+                key = str(args)+str(kwargs)
+                if key in arglist:
+                    print(key + 'found in cache')
+                    return arglist[key]
+                else:
+                    arglist[key] = f(args[0])
+                    #logfile.write(f.__name__ + key + ' = ' + str(arglist[key]) + '\n')
+                    return arglist[key]
             return f(*args,**kwargs)
         return wrapper
     return real
 
-theargs = set()
-
-@storeargs(theargs)
+arglist = {}
+@track
+@log(arglist)
 def fib(n):
     return n if n in (0,1) else fib(n-1) + fib(n-2)
 
-print(fib(10))
-print(theargs)
-
+print(f'{fib(10)}, calls = {fib.count}')

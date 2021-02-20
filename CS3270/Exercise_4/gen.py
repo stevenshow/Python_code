7bc49d34a1ee8bbd9e8416358650c55c4dc04c9a
stevenshow <steven.schoebinger@gmail.com>
2021-02-20 00:17:34 -0700

    Got pylint to 10/10

--- CS3270/Exercise_4/gen.py
+++ CS3270/Exercise_4/gen.py
@@ -1,12 +1,16 @@
-from itertools import islice
+'''Uses generators to process a string a character at a time and
+return a string based on pre-set conditions with digits.
+Created by: Steven Schoebinger 2/19/2021'''
+# pylint: disable=invalid-name
 
 the_string = 'A2B5E3426FG0ZYW3210PQ89R'
 
 def produce(my_string):
+    '''Processes the string and yields to new string'''
     how_many = 0
     last_was_digit = False
     for x in my_string:
-        if x.isdigit() and last_was_digit == False:
+        if x.isdigit() and not last_was_digit:
             how_many = int(x) + 1
             last_was_digit = True
         elif last_was_digit:
@@ -16,6 +20,7 @@ def produce(my_string):
             yield x
 
 def consume1(my_iter):
+    '''Grabs the processed generator stream and prints groups of 3'''
     stream = []
     #while my_iter:
     for x in my_iter:
@@ -29,22 +34,3 @@ def consume1(my_iter):
     print(''.join(stream))
 
 consume1(produce(the_string))
-
-#p = produce(the_string)
-#for s in p: print(s,end=' ')
-
-
-#def consume(my_iter):
-#    characters = ''
-#    count = 0
-#    for x in my_iter:
-#        if len(x) < 3:
-#            characters = x
-#        if len(characters) == 3:
-#            print(characters)
-#            characters = 0
-#        if len(x) == 3:
-#            print(x)
-#        elif len(x) > 3:
-#            print(x[0:3])
-#            characters = x[3:]

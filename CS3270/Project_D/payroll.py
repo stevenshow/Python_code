469f07afb8b26f525d2973693395d39f61a4898a
stevenshow <steven.schoebinger@gmail.com>
2021-03-14 23:59:54 -0600

    created barebones from the UML diagram

--- CS3270/Project_D/payroll.py
+++ CS3270/Project_D/payroll.py
@@ -22,13 +22,64 @@ class Employee:
     def issue_payment(self):
         pass
 
+#Is this necessary?  
 class classification(Employee):
     def __init__(self, id, name, address, city, state, zipcode, classification, paymethod):
         self.classification = classification   
-        Employee.__init__(self, id, name, address, city, state, zipcode, classification, paymethod)
+        super.__init__(self, id, name, address, city, state, zipcode, classification, paymethod)
+    
     def compute_pay(self):
         pass
 
 class Salaried(classification):
     def __init__(self, id, name, address, city, state, zipcode, classification, paymethod):
         super().__init__(id, name, address, city, state, zipcode, classification, paymethod)
+    
+    def compute_pay(self):
+        pass
+
+class Hourly(classification):
+    def __init__(self, id, name, address, city, state, zipcode, classification, paymethod):
+        super().__init__(id, name, address, city, state, zipcode, classification, paymethod)
+    hourly_rate = 0
+    timecards = []
+
+    def compute_pay(self):
+        pass
+
+    def add_timecards(self, hours):
+        pass
+
+class Commissioned(Salaried):
+    def __init__(self, id, name, address, city, state, zipcode, classification, paymethod):
+        super().__init__(id, name, address, city, state, zipcode, classification, paymethod)
+    receipts = []
+    
+    def add_receipt(self, amount):
+        pass
+
+    def compute_pay(self):
+        pass
+
+
+
+
+
+
+
+class PayMethod():
+    Employee
+
+    def issue(self, pay):
+        pass
+
+class DirectMethod(PayMethod):
+    route = ""
+    account = ""
+
+    def issue(self, pay):
+        pass
+
+class MailMethod(PayMethod):
+    def issue(self, pay):
+        pass

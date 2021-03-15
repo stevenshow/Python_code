0fcae82aa014a6ae44c0e6dee9e83b3beb4c4939
stevenshow <steven.schoebinger@gmail.com>
2021-03-14 23:32:24 -0600

    .

--- Inheritance.py
+++ Inheritance.py
@@ -1,7 +1,30 @@
 import string
 
-class Cat():
-    int age
-    int loafness
-    string name
+class Employee:
+    def __init__(self, id, name, address, city, state, zipcode):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+    
+    def make_hourly(hourly_rate):
+        pass
+
+    def make_salaried(salary):
+        pass
+
+    def make_commission(salary, rate):
+        pass
+
+    def mail_method():
+        pass
+
+    def direct_method(route, account):
+        pass
+
+    def issue_payment():
+        pass
+
     

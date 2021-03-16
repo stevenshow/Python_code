59483f2fe3ae09cc621ad8470d0fbc84257b2927
stevenshow <steven.schoebinger@gmail.com>
2021-03-16 01:28:58 -0600

    formatted for eye candy

--- CS3270/Project_D/project_d.py
+++ CS3270/Project_D/project_d.py
@@ -1,61 +1,65 @@
 '''
     project_d.py: Illustrates the payroll module.
 '''
-from payroll import *
-import shutil
 import os
+import shutil
 from collections import namedtuple
 from csv import reader
+
+from payroll import *
+
 path = os.path.dirname(__file__)
 PAY_LOGFILE = 'paylog.txt'
 employees = {}
 
+
 def load_employees():
-    #list contains Employee objects
+    # list contains Employee objects
     #Hourly = 1
     #Salary = 2
     #Commissioned = 3
-    #Direct Deposit = 1
-    #Mail Method = 2
+    # Direct Deposit = 1
+    # Mail Method = 2
     with open(path + '/employees.csv', 'r') as f:
-        #Get first line of csv and use it to set up named tuple
+        # Get first line of csv and use it to set up named tuple
         csv_reader = reader(f)
         emps = namedtuple("Employee", next(csv_reader))
-        #._make() is used to return a namedtuple() from the iterable passed as argument
+        # ._make() is used to return a namedtuple() from the iterable passed as argument
         for emp in map(emps._make, csv_reader):
             if emp.Classification == '1':
                 hourly = Hourly(emp.Hourly)
-                employees[emp.ID] = Employee(emp.ID, emp.Name, emp.Address, emp.City, emp.State, emp.Zip, hourly, emp.PayMethod)
+                employees[emp.ID] = Employee(
+                    emp.ID, emp.Name, emp.Address, emp.City, emp.State, emp.Zip, hourly, emp.PayMethod)
                 if emp.PayMethod == '1':
-                    direct = DirectMethod(employees[emp.ID], emp.Route, emp.Account)
+                    direct = DirectMethod(
+                        employees[emp.ID], emp.Route, emp.Account)
                     employees[emp.ID].PayMethod = direct
                 else:
                     mail = MailMethod(employees[emp.ID])
                     employees[emp.ID].PayMethod = mail
             elif emp.Classification == '2':
                 salary = Salaried(emp.Salary)
-                employees[emp.ID] = Employee(emp.ID, emp.Name, emp.Address, emp.City, emp.State, emp.Zip, salary, emp.PayMethod)
+                employees[emp.ID] = Employee(
+                    emp.ID, emp.Name, emp.Address, emp.City, emp.State, emp.Zip, salary, emp.PayMethod)
                 if emp.PayMethod == '1':
-                    direct = DirectMethod(employees[emp.ID], emp.Route, emp.Account)
+                    direct = DirectMethod(
+                        employees[emp.ID], emp.Route, emp.Account)
                     employees[emp.ID].PayMethod = direct
                 else:
                     mail = MailMethod(employees[emp.ID])
                     employees[emp.ID].PayMethod = mail
             elif emp.Classification == '3':
                 commission = Commissioned(emp.Salary, emp.Commission)
-                employees[emp.ID] = Employee(emp.ID, emp.Name, emp.Address, emp.City, emp.State, emp.Zip, salary, emp.PayMethod)
+                employees[emp.ID] = Employee(
+                    emp.ID, emp.Name, emp.Address, emp.City, emp.State, emp.Zip, salary, emp.PayMethod)
                 if emp.PayMethod == '1':
-                    direct = DirectMethod(employees[emp.ID], emp.Route, emp.Account)
+                    direct = DirectMethod(
+                        employees[emp.ID], emp.Route, emp.Account)
                     employees[emp.ID].PayMethod = direct
                 else:
                     mail = MailMethod(employees[emp.ID])
                     employees[emp.ID].PayMethod = mail
 
-def find_employee_by_id(id):
-    #loop through employee list.  Check employee.id and return Employee object
-    if id in employees:
-        return employees[id]
-    return -1
 
 def process_timecards():
     with open(path + '/timecards.txt', 'r') as f:
@@ -63,18 +67,29 @@ def process_timecards():
         for hour_list in csv_reader:
             id = hour_list.pop(0)
             employees[id].classification.timecards = hour_list
-            
+
+
 def process_receipts():
     with open(path + '/receipts.txt', 'r') as f:
         csv_reader = reader(f)
         for receipt_list in csv_reader:
             id = receipt_list.pop(0)
             employees[id].classification.receipts = receipt_list
+
+
 def run_payroll():
     if os.path.exists(PAY_LOGFILE):
         os.remove(PAY_LOGFILE)
     for emp in employees:
         emp.issue_payment()
+
+
+def find_employee_by_id(id):
+    # loop through employee list.  Check employee.id and return Employee object
+    if id in employees:
+        return employees[id]
+    return -1
+
 
 def main():
     load_employees()
@@ -112,6 +127,7 @@ def main():
     clas.add_timecard(8.0)
     clas.add_timecard(8.0)
     emp.issue_payment()
+
 
 if __name__ == '__main__':
     main()

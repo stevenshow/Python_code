4c42abba7887fe9a33c3fc3f83e4795b2a143e6d
stevenshow <steven.schoebinger@gmail.com>
2021-03-17 22:44:23 -0600

    Fixed naming of Project_E

--- CS3270/Exercise_5/view.py
+++ CS3270/Exercise_5/view.py
@@ -1,23 +1,27 @@
 import sys
+import os
 # [X] Get command line arguments
 # [X] Get input from user to know where to naviage the page
 # [] Implement commands [u,d,t,b,#,q]
 # [] Enter is a down command
 # [] Don't allow viewing empty files
 # [] Read file in only the pre-determined amount, not the entire file
+
+path = os.path.dirname(__file__)
 view_size = 25
-fname = sys.argv[1]
+fname = path + '/yankee.txt'
+#fname = sys.argv[1]
+
 # This is optional, if I can't figure out how to make sure this variable exists, remove it all together
-view_size = sys.argv[2]
+#view_size = sys.argv[2]
 
+def view(args):
+    #print(fname)
+    #print(view_size)
+    with open(fname, 'r') as f:
+        print(f.seek(20))
+            #x = input("Please enter something: ")
+            #print(x)
 
-def view(fname, view_size=25):
-    print(fname)
-    print(view_size)
-    while True:
-        x = input("Please enter something: ")
-        print(x)
-    # print(fname)
-
-
-view(fname, view_size)
+if __name__ == '__main__':
+    view(sys.argv)

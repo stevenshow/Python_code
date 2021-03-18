6d626227cdf8d0655e711dfee0cf3c2ca00e88ff
stevenshow <steven.schoebinger@gmail.com>
2021-03-17 23:46:04 -0600

    started the Up and Down commands

--- CS3270/Exercise_5/view.py
+++ CS3270/Exercise_5/view.py
@@ -2,6 +2,7 @@ import sys
 import os
 # [X] Get command line arguments
 # [X] Get input from user to know where to naviage the page
+# [X] Get files offsets to sort pages accordingly
 # [] Implement commands [u,d,t,b,#,q]
 # [] Enter is a down command
 # [] Don't allow viewing empty files
@@ -19,9 +20,54 @@ def view(args):
     #print(fname)
     #print(view_size)
     with open(fname, 'r') as f:
-        print(f.seek(20))
-            #x = input("Please enter something: ")
-            #print(x)
+        #create list of offsets to use for naviagtion
+        offsets = [0]
+        cursor = (0)
+        command = ''
+        while f.readline():
+            offsets.append(f.tell())
+        offsets.pop() # Remove EOF file POS
+        #Print initial view
+        for x in range(0, 20):
+            f.seek(offsets[x])
+            print(f.readline(), end='')
+        cursor = offsets.index(f.tell())
+        print('\n')    
+        print(len(offsets))
+        while command != 'q':
+            command = input('Command [u,d,t,b,#,q]: ')
+            # Moves one page down; if at botton wrap to top
+            if command == 'd':
+                if cursor != offsets[-20]:
+                    for x in range(cursor, cursor+20):
+                        f.seek(offsets[x])
+                        print(f.readline(), end='')
+                    cursor = offsets.index(f.tell())
+                else:
+                    # Takes you to the first page if at the last page
+                    for x in range(0, 20):
+                        f.seek(offsets[x])
+                        print(f.readline(), end='')
+                    cursor = offsets.index(f.tell())    
+            
+            if command == 'u':
+                if cursor != offsets[0]:
+                    for x in range(cursor -20, cursor):
+                        f.seek(offsets[x])
+                        print(f.readline(), end='')
+                    cursor = offsets.index(f.tell())
+                else:
+                    for x in range(offsets[-20], offsets[-1]):
+                        f.seek(offsets[x])
+                        print(f.readline(), end='')
+                    cursor = offsets.index(f.tell())
+
+
+
+            #Gets last index location for use with user input
+
+        #x = input("Please enter something: ")
+        #print(x)
 
 if __name__ == '__main__':
     view(sys.argv)

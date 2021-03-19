2df5a271a36a7ce22cd919c2c3108e647ff997f2
stevenshow <steven.schoebinger@gmail.com>
2021-03-18 23:19:34 -0600

    Finished all commands and fixed the paging issue.

--- CS3270/Exercise_5/view.py
+++ CS3270/Exercise_5/view.py
@@ -1,20 +1,20 @@
 import sys
 import os
-# [X] Get command line arguments
 # [X] Get input from user to know where to naviage the page
 # [X] Get files offsets to sort pages accordingly
 # [X] Read file in only the pre-determined amount, not the entire file
-# [] Implement commands [u,d,t,b,#,q]
+# [X] Implement commands [u,d,t,b,#,q]
 # [X] d command
 # [X] u command
-# [] t command
-# [] b command
-# [] # command
-# [] Enter is a down command
+# [X] t command
+# [X] b command
+# [X] # command
+# [X] Enter is a down command
+# [] Get command line arguments for file reading and view size
 # [] Don't allow viewing empty files
 
 path = os.path.dirname(__file__)
-view_size = 25
+view_size = 20
 fname = path + '/yankee.txt'
 #fname = sys.argv[1]
 
@@ -48,47 +48,49 @@ def view(args):
 
         # Print initial view
         print(f'[Page {current_page + 1}]:')
-        for x in range(0, 20):
+        for x in range(0, view_size):
             f.seek(offsets[x])
             print(f.readline(), end='')
         print('\n')
 
         # Gets list of pages with their respectice indicies
         start = 0
-        stop = 20
-        for x in range(int(len(offsets)/20)):
+        stop = view_size
+        for x in range(int(len(offsets)/view_size)):
             pages.append(offsets[start:stop])
-            start += 20
-            stop += 20
+            start += view_size
+            stop += view_size
         current_page = 1
 
         # Start loop to get and process input
         while command != 'q':
             command = input('Command [u,d,t,b,#,q]: ')
+            
             # Moves one page down; if at botton wrap to the top
-            if command == 'd':
-                if current_page < len(pages) - 1:
-                    print(f'[Page {current_page + 1}]:')
-                    for x in range(len(pages[current_page+1])):
-                        f.seek(pages[current_page+1][x])
+            if command == 'd' or command == '':
+                if current_page <= len(pages) - 1:
+                    current_page += 1
+                    print(f'[Page {current_page}]:', current_page)
+                    for x in range(len(pages[current_page-1])):
+                        f.seek(pages[current_page-1][x])
                         print(f.readline(), end='')
                     print('\n')
-                    current_page += 1
                 # If the current page is the last page
                 #current_page should exit on 0
                 else:
-                    current_page = 0
-                    print(f'[Page {current_page + 1}]:')
-                    for x in range(len(pages[current_page])):
-                        f.seek(pages[current_page][x])
+                    current_page = 1
+                    print(f'[Page {current_page}]:', current_page)
+                    for x in range(len(pages[current_page-1])):
+                        f.seek(pages[current_page-1][x])
                         print(f.readline(), end='')
                     print('\n')
-                    current_page = 0
+                    #current_page = 1
+            
             # Moves view up 1 page; If at the top, wrap to the bottom
             if command == 'u':
                 if current_page != 0 and current_page != 1:
                     current_page -= 1
-                    print(f'[Page {current_page}]:')
+                    print(f'[Page {current_page}]:', current_page)
                     for x in range(len(pages[current_page-1])):
                         f.seek(pages[current_page-1][x])
                         print(f.readline(), end='')
@@ -96,26 +98,27 @@ def view(args):
                 # If the current page is the first page
                 # current_page should exit on 100
                 else:
-                    current_page = len(pages) - 1
-                    print(f'[Page {current_page + 1}]:')
+                    current_page = len(pages)
+                    print(f'[Page {current_page}]:', current_page)
                     for x in range(len(pages[-1])):
                         f.seek(pages[-1][x])
                         print(f.readline(), end='')
                     print('\n')
                     current_page = len(pages)
+            
             # Moves to the top page
             if command == 't':
-                current_page = 0
-                print(f'[Page {current_page + 1}]:')
-                for x in range(len(pages[0])):
-                    f.seek(pages[0][x])
+                current_page = 1
+                print(f'[Page {current_page}]:', current_page)
+                for x in range(len(pages[current_page-1])):
+                    f.seek(pages[current_page-1][x])
                     print(f.readline(), end='')
                 print('\n')
 
             # Moved to the bottom page
             if command == 'b':
-                current_page = len(pages) - 1
-                print(f'[Page {current_page + 1}]:')
+                current_page = len(pages)
+                print(f'[Page {current_page}]:', current_page)
                 for x in range(len(pages[-1])):
                     f.seek(pages[-1][x])
                     print(f.readline(), end='')
@@ -124,16 +127,12 @@ def view(args):
 
             # Move to the specified page number
             if command.isdigit() and int(command) <= len(pages) and int(command) > 0:
-                current_page = int(command) - 1
-                print(f'[Page {current_page + 1}]:')
-                for x in range(len(pages[current_page])):
-                    f.seek(pages[current_page][x])
+                current_page = int(command)
+                print(f'[Page {current_page}]:', current_page)
+                for x in range(len(pages[current_page-1])):
+                    f.seek(pages[current_page-1][x])
                     print(f.readline(), end='')
                 print('\n')
-                current_page = int(command)
-
-# Issues:
-# Goine down on page 1 and then up on page 100 causes the page to be -1
 
 
 if __name__ == '__main__':

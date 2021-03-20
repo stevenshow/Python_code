85db8183302e43a3f751cd4c91e65eaf7028b881
stevenshow <steven.schoebinger@gmail.com>
2021-03-19 21:38:49 -0600

    Finished program.  Moved all commands into functions

--- CS3270/Exercise_5/view.py
+++ CS3270/Exercise_5/view.py
@@ -1,139 +1,158 @@
+'''Command line program that takes a file name and a view size
+and then outputs the file 1 page at a time based on the view size
+Created by: Steven Schoebinger 03/19/2021'''
 import sys
 import os
+import math
+# pylint: disable=invalid-name
 # [X] Get input from user to know where to naviage the page
 # [X] Get files offsets to sort pages accordingly
 # [X] Read file in only the pre-determined amount, not the entire file
-# [X] Implement commands [u,d,t,b,#,q]
-# [X] d command
-# [X] u command
-# [X] t command
-# [X] b command
-# [X] # command
-# [X] Enter is a down command
-# [] Get command line arguments for file reading and view size
-# [] Don't allow viewing empty files
+# [X] Implement commands [u,d,t,b,#,q]; Enter is a down command
+# [X] Get command line arguments for file reading and view size
+# [X] Don't allow viewing empty files
 
-path = os.path.dirname(__file__)
+# Gets directory name in either IDE or Command Line; Sets defaults if nothing is passed
+path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/'
+fname = path + 'yankee.txt'
 view_size = 20
-fname = path + '/yankee.txt'
-#fname = sys.argv[1]
 
-# This is optional, if I can't figure out how to make sure this variable exists, remove it all together
-#view_size = sys.argv[2]
+# If a command line argument is passed
+if len(sys.argv) > 1:
+    fname = path + sys.argv[1]
+    try:
+        view_size = int(sys.argv[2])
+    # If there is no view size passed
+    except IndexError:
+        view_size = 20
 
+def down(f, current_page, pages):
+    '''Moves one page down; if at botton wrap to the top; returns current page after print'''
+    command = 'd'
+    if command in ['', 'd']:
+        if current_page <= len(pages) - 1:
+            current_page += 1
+            print(f'[Page {current_page}]:')
+            for x in range(len(pages[current_page-1])):
+                f.seek(pages[current_page-1][x])
+                print(f.readline(), end='')
+            print('\n')
+        # If the current page is the last page
+        else:
+            current_page = 1
+            print(f'[Page {current_page}]:')
+            for x in range(len(pages[current_page-1])):
+                f.seek(pages[current_page-1][x])
+                print(f.readline(), end='')
+            print('\n')
+    return current_page
 
-# How to pad the last lines of the file to make the view correct on the last page
-# Need to figure out how to append the newline to the file, does doing a newline eat up a whole readline?
-# while x % 25 != 0:
-#x += 1
-
-def view(args):
-    with open(fname, 'r+') as f:
-        # create list of offsets to use for naviagtion
-        current_page = 0
-        offsets = [0]
-        command = ''
-        pages = []
-
-        while f.readline():
-            offsets.append(f.tell())
-        offsets.pop()  # Remove EOF file POS
-        f.seek(offsets.pop())
-        print(f.readline(), end='')
-
-        # Add newlines until the offsets mod viewsize = 0 so the views are all equal
-        # while len(offsets) % 20 != 0:
-        #     offsets.append('\n')
-        #     f.write('\n')
-
-        # Print initial view
-        print(f'[Page {current_page + 1}]:')
-        for x in range(0, view_size):
-            f.seek(offsets[x])
+def up(f, current_page, pages):
+    '''Moves up one page; if at top wrap to the bottom; returns current page after print'''
+    if current_page not in [0,1]:
+        current_page -= 1
+        print(f'[Page {current_page}]:')
+        for x in range(len(pages[current_page-1])):
+            f.seek(pages[current_page-1][x])
             print(f.readline(), end='')
         print('\n')
+    # If the current page is the first page
+    else:
+        current_page = len(pages)
+        print(f'[Page {current_page}]:')
+        for x in range(len(pages[-1])):
+            f.seek(pages[-1][x])
+            print(f.readline(), end='')
+        print('\n')
+        current_page = len(pages)
+    return current_page
 
-        # Gets list of pages with their respectice indicies
-        start = 0
-        stop = view_size
-        for x in range(int(len(offsets)/view_size)):
-            pages.append(offsets[start:stop])
-            start += view_size
-            stop += view_size
-        current_page = 1
+def top(f, current_page, pages):
+    '''Moves to the top page; returns current page after print'''
+    current_page = 1
+    print(f'[Page {current_page}]:')
+    for x in range(len(pages[current_page-1])):
+        f.seek(pages[current_page-1][x])
+        print(f.readline(), end='')
+    print('\n')
+    return current_page
 
-        # Start loop to get and process input
-        while command != 'q':
-            command = input('Command [u,d,t,b,#,q]: ')
-            
-            # Moves one page down; if at botton wrap to the top
-            if command == 'd' or command == '':
-                if current_page <= len(pages) - 1:
-                    current_page += 1
-                    print(f'[Page {current_page}]:', current_page)
-                    for x in range(len(pages[current_page-1])):
-                        f.seek(pages[current_page-1][x])
-                        print(f.readline(), end='')
-                    print('\n')
-                # If the current page is the last page
-                #current_page should exit on 0
-                else:
-                    current_page = 1
-                    print(f'[Page {current_page}]:', current_page)
-                    for x in range(len(pages[current_page-1])):
-                        f.seek(pages[current_page-1][x])
-                        print(f.readline(), end='')
-                    print('\n')
-                    #current_page = 1
-            
-            # Moves view up 1 page; If at the top, wrap to the bottom
-            if command == 'u':
-                if current_page != 0 and current_page != 1:
-                    current_page -= 1
-                    print(f'[Page {current_page}]:', current_page)
-                    for x in range(len(pages[current_page-1])):
-                        f.seek(pages[current_page-1][x])
-                        print(f.readline(), end='')
-                    print('\n')
-                # If the current page is the first page
-                # current_page should exit on 100
-                else:
-                    current_page = len(pages)
-                    print(f'[Page {current_page}]:', current_page)
-                    for x in range(len(pages[-1])):
-                        f.seek(pages[-1][x])
-                        print(f.readline(), end='')
-                    print('\n')
-                    current_page = len(pages)
-            
-            # Moves to the top page
-            if command == 't':
-                current_page = 1
-                print(f'[Page {current_page}]:', current_page)
-                for x in range(len(pages[current_page-1])):
-                    f.seek(pages[current_page-1][x])
-                    print(f.readline(), end='')
-                print('\n')
+def bottom(f, current_page, pages):
+    '''Moves to the bottom page; returns current page after print'''
+    current_page = len(pages)
+    print(f'[Page {current_page}]:')
+    for x in range(len(pages[-1])):
+        f.seek(pages[-1][x])
+        print(f.readline(), end='')
+    print('\n')
+    current_page = len(pages)
+    return current_page
 
-            # Moved to the bottom page
-            if command == 'b':
-                current_page = len(pages)
-                print(f'[Page {current_page}]:', current_page)
-                for x in range(len(pages[-1])):
-                    f.seek(pages[-1][x])
-                    print(f.readline(), end='')
-                print('\n')
-                current_page = len(pages)
+def to_page(f, current_page, pages, command):
+    '''Moves to the page passed by user; returns current page after print'''
+    current_page = int(command)
+    print(f'[Page {current_page}]:')
+    for x in range(len(pages[current_page-1])):
+        f.seek(pages[current_page-1][x])
+        print(f.readline(), end='')
+    print('\n')
+    return current_page
 
-            # Move to the specified page number
-            if command.isdigit() and int(command) <= len(pages) and int(command) > 0:
-                current_page = int(command)
-                print(f'[Page {current_page}]:', current_page)
-                for x in range(len(pages[current_page-1])):
-                    f.seek(pages[current_page-1][x])
-                    print(f.readline(), end='')
-                print('\n')
+def view(file, size):
+    '''Finds the line offsets of the file and then places them in a list.
+    Uses that list to create pages based on view_size'''
+    # Checks if the file is empty
+    if os.path.getsize(file) != 0:
+        with open(file, 'r+') as f:
+            # create list of offsets to use for naviagtion
+            current_page = 0
+            offsets = [0]
+            command = ''
+            pages = []
 
+            # Gets list of line offsets
+            while f.readline():
+                offsets.append(f.tell())
+            f.seek(offsets.pop())
+            print(f.readline(), end='')
+
+            # Print initial view
+            print(f'[Page {current_page+1}]:')
+            for x in range(0, size):
+                f.seek(offsets[x])
+                print(f.readline(), end='')
+            print('\n')
+
+            # Gets list of pages with their respective indicies
+            start = 0
+            stop = size
+            for x in range(math.ceil(len(offsets)/size)):
+                pages.append(offsets[start:stop])
+                start += size
+                stop += size
+            current_page = 1
+
+            # Start loop to get and process input
+            while command != 'q':
+                command = input('Command [u,d,t,b,#,q]: ')
+                # Moves one page down; if at botton wrap to the top
+                if command == 'd':
+                    current_page = down(f, current_page, pages)
+                # Moves view up 1 page; If at the top, wrap to the bottom
+                if command == 'u':
+                    current_page = up(f,current_page, pages)
+                # Moves to the top page
+                if command == 't':
+                    current_page = top(f, current_page, pages)
+                # Moves to the bottom page
+                if command == 'b':
+                    current_page = bottom(f, current_page, pages)
+                # Move to the specified page number
+                if command.isdigit() and int(command) <= len(pages) and int(command) > 0:
+                    current_page = to_page(f, current_page, pages, command)
+    else:
+        print('File is empty!')
+        sys.exit()
 
 if __name__ == '__main__':
-    view(sys.argv)
+    view(fname, view_size)

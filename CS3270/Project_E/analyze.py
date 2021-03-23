a2cb4cf2a6efc14dca2ae4f88e6c6872385ac2b2
stevenshow <steven.schoebinger@gmail.com>
2021-03-22 22:24:05 -0600

    Added project TODOs

---
+++ CS3270/Project_E/analyze.py
@@ -0,0 +1,10 @@
+import array as arr
+
+# [] Check for any .dat files in directory
+# [] Make .ini parser
+# [] Make .dat parser
+# [] Create jagged data and plot (Find pulse) pt_1
+# [] Check for piggyback pt_2
+# [] Create smoothed data and plot (Find area) pt_1
+# [] Area is the sum of the of the values starting at the pulse start and going for width values, or till the start of next pulse
+ 

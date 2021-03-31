b823dde9fc90495720b9270fdef2424ebe61521a
stevenshow <steven.schoebinger@gmail.com>
2021-03-30 22:49:09 -0600

    Starting on Exercise 6

---
+++ practice/tkinter/in_class_practice.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import random
+import os
+import string
+# Let's make a GUI that can input a file location and then open that file if it exists, throw an error if not. 
+path = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+def change_text():
+    var.set(random.randint(10, 100))
+
+def read_file():
+    lines= []
+    try:
+        with open(path + 'text.txt', 'r') as f:
+            for ele in f.readlines():
+                lines.append(ele)
+    except:
+        var.set('Could not find file.')
+    var.set(''.join(lines))
+
+root = Tk()
+toolbar = Frame(root)
+toolbar.pack()
+
+# Trying to place in center of screen
+windowWidth = root.winfo_reqwidth()
+windowHeight = root.winfo_reqheight()
+posRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
+posDown = int(root.winfo_screenmmheight() - windowHeight/2)
+
+b_quit = Button(toolbar, text='Quit', fg='red', command=quit)
+var = StringVar()
+var.set('Click to get random number')
+b = Button(toolbar, text='Change Number', command=change_text, padx='5', pady='5')
+r = Button(toolbar, text='Read text.txt file', command=read_file, padx='5', pady='5')
+w = Label(root, textvariable=var)
+r.pack(side=LEFT)
+b.pack(side=LEFT)
+w.pack()
+b_quit.pack(side=RIGHT)
+root.title('Window')
+root.geometry("300x300+{}+{}".format(posRight, posDown))
+root.mainloop()

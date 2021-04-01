d6c1d25f95a10f69aa16356a1a98fe682d9f7aa6
stevenshow <steven.schoebinger@gmail.com>
2021-04-01 12:54:45 -0600

    Started Exercise 7

---
+++ CS3270/Exercise_7/web_link.py
@@ -0,0 +1,45 @@
+import pprint
+
+class HayStack():
+    def __init__(self, url, depth):
+        super().__init__()
+    
+    def lookup():
+        pass
+
+    def compute_ranks(self,graph):
+        d = 0.85
+        # probability that surfer will bail
+        numloops = 10
+        ranks = {}
+        npages = len(graph)
+        for page in graph:
+            ranks[page] = 1.0 / npages
+        for _ in range(0, numloops):
+            newranks = {}
+            for page in graph:
+                newrank = (1 - d) / npages
+                for url in graph:
+                    if page in graph[url]: # this url links to page
+                        newrank += d*ranks[url]/len(graph[url])
+                newranks[page] = newrank
+            ranks = newranks
+        self.ranks = ranks
+
+
+
+
+if __name__ == '__main__':
+    engine = HayStack('http://freshsources.com/page1.html',4)
+    for w in ['pages','links','you','have','I']:
+        print(w)
+        pprint.pprint(engine.lookup(w))
+        print()
+        print('index:')
+        pprint.pprint(engine.index)
+        print()
+        print('graph:')
+        pprint.pprint(engine.graph)
+        print()
+        print('ranks:')
+        pprint.pprint(engine.ranks)

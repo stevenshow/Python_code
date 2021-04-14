ffbca46327d2a151cad6409ddd9e85230789ff03
stevenshow <steven.schoebinger@gmail.com>
2021-04-14 01:46:56 -0600

    Added filesize summation

--- CS3270/Exercise_8/flag_seq.py
+++ CS3270/Exercise_8/flag_seq.py
@@ -1,8 +1,7 @@
-# [] Download all flag files into a local directory named flags
-# [] Report the number of bytes downloaded•
-# [] Report the execution time of the script(using time.perf_counter())•
-# [] Report the CPU time of the script (using time.process_time())
-
+# [X] Download all flag files into a local directory named flags
+# [X] Report the number of bytes downloaded•
+# [X] Report the execution time of the script(using time.perf_counter())•
+# [X] Report the CPU time of the script (using time.process_time())
 import os
 import requests
 from time import perf_counter
@@ -18,14 +17,18 @@ with open(txt_path + 'flags.txt', 'r') as f:
 def get_flags():
     start = perf_counter()
     cpu_start = process_time()
+    sum_filesizes = 0
     for country in flags:        
         flag_name = country + '.jpg'
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'}
         image = requests.get('https://www.sciencekids.co.nz/images/pictures/flags96/' + flag_name,headers=headers)
+        print(image.headers['Content-Length'])
+        sum_filesizes += int(image.headers["Content-Length"])
         open(img_path + flag_name, 'wb+').write(image.content)
     cpu_end = process_time()
     end = perf_counter()
     print(f'Elapsed CPU Time: {round(cpu_end-cpu_start, 2)} seconds.')
     print(f'Elapsed Script Time: {round(end-start,2)} seconds.')
-
+    print(f'Amount of bytes downloaded: {round(sum_filesizes/1024, 3)} Kbs')
+    
 get_flags()

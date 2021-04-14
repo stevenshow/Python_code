2365540b61b70c66cb0fc03b3b9908bb06164208
stevenshow <steven.schoebinger@gmail.com>
2021-04-14 01:09:20 -0600

    Completed Sequential Script

--- CS3270/Exercise_8/flag_seq.py
+++ CS3270/Exercise_8/flag_seq.py
@@ -4,15 +4,9 @@
 # [] Report the CPU time of the script (using time.process_time())
 
 import os
-import smtplib
-import ssl
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import requests
 from time import perf_counter
 
-start = perf_counter()
 
 img_path = os.path.dirname(__file__) + '/flags/'
 txt_path = os.path.dirname(__file__) + '/'
@@ -22,12 +16,13 @@ with open(txt_path + 'flags.txt', 'r') as f:
     flags = f.read().splitlines()
 
 def get_flags():
+    start = perf_counter()
     for country in flags:        
         flag_name = country + '.jpg'
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'}
         image = requests.get('https://www.sciencekids.co.nz/images/pictures/flags96/' + flag_name,headers=headers)
         open(img_path + flag_name, 'wb+').write(image.content)
+    end = perf_counter()
+    print(f'Elapsed Time: {round(end-start,2)} seconds.')
 
 get_flags()
-end = perf_counter()
-print('Elapsed time: ', end-start)

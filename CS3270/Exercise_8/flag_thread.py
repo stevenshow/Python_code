3de2166caaa94ab217bb4b32c98c5a665dd3f699
stevenshow <steven.schoebinger@gmail.com>
2021-04-14 01:09:29 -0600

    Completed Threading Script

---
+++ CS3270/Exercise_8/flag_thread.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from time import perf_counter
+import concurrent.futures
+
+img_path = os.path.dirname(__file__) + '/flags/'
+txt_path = os.path.dirname(__file__) + '/'
+
+with open(txt_path + 'flags.txt', 'r') as f:
+    flags = f.read().splitlines()
+
+def get_flag(country):
+    flag_name = country+ '.jpg'
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'}
+    image = requests.get('https://www.sciencekids.co.nz/images/pictures/flags96/' + flag_name,headers=headers)
+    open(img_path + flag_name, 'wb+').write(image.content)
+
+
+def t_pool_submit():
+    start = perf_counter()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(get_flag, flags)
+
+    end = perf_counter()
+    print(f'Elapsed Time: {round(end-start,2)} seconds.')
+
+t_pool_submit()

382b9a28bb56ef7fcc5a89da856ca16d6b3e5ed8
stevenshow <steven.schoebinger@gmail.com>
2021-04-20 17:51:53 -0600

    Completed Part 1 of Project F

--- CS3270/Project_F/readfiles.py
+++ CS3270/Project_F/readfiles.py
@@ -1,3 +1,52 @@
+'''Script that takes a command line argument of the directory name that you would like
+to traverse and store each file in the created database.
+Created by: Steven Schoebinger 04/20/2021'''
 import sqlite3
+import os, sys
+                              
+py_path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/'
+path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/' + sys.argv[1]
+db_path = os.path.join(py_path, 'filesdb')
 
-conn = sqlite3.connect('filesdb')
+# Create new database each time so the data is always correct
+if os.path.exists(db_path):
+    os.remove(db_path)
+
+connection = sqlite3.connect(db_path)
+print('filesdb created')
+cursor = connection.cursor()
+
+cursor.execute('''CREATE TABLE files (
+            ext TEXT,
+            path TEXT,
+            fname TEXT
+            )''')
+
+# os.walk will go through each directory and collect all files
+for root, dirs, files in os.walk(path):
+    # root will get the path to the files
+    for file in files:
+        if len(file.split('.')) > 1 and file.split('.')[0] != '':
+            fname = file.split('.')[0]
+            ext = file.split('.')[1]
+            path = root
+            cursor.execute('INSERT INTO files VALUES (?,?,?)', (ext, path, fname))
+        else:
+            fname = file
+            ext = None
+            path = root
+            cursor.execute('INSERT INTO files VALUES (?,?,?)', (ext, path, fname))
+
+# Get all data from database
+cursor.execute('SELECT * FROM files')
+rows = cursor.fetchall()
+
+# Print query to .txt file
+with open(py_path + 'files-part1.txt', 'a+') as f:
+    for row in rows:
+        f.write(str(row) + '\n')
+
+
+connection.commit()
+
+connection.close()

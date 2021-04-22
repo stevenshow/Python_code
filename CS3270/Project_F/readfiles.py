12cb6f784b5752d8625c57db1070d2878a6c620f
stevenshow <steven.schoebinger@gmail.com>
2021-04-21 23:09:25 -0600

    Placed inside of main()

--- CS3270/Project_F/readfiles.py
+++ CS3270/Project_F/readfiles.py
@@ -6,56 +6,61 @@ import sqlite3
 import sys
 
 # pylint: disable=invalid-name                             
-py_path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/'
-path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/' + sys.argv[1]
-db_path = os.path.join(py_path, 'filesdb')
 
-# Create new database each time so the data is always correct
-if os.path.exists(db_path):
-    os.remove(db_path)
+def main():
 
-connection = sqlite3.connect(db_path)
-print('filesdb created')
-cursor = connection.cursor()
+    py_path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/'
+    path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/' + sys.argv[1]
+    db_path = os.path.join(py_path, 'filesdb')
+    # Create new database each time so the data is always correct
+    if os.path.exists(db_path):
+        os.remove(db_path)
 
-cursor.execute('''CREATE TABLE files (
-            ext TEXT,
-            path TEXT,
-            fname TEXT
-            )''')
+    connection = sqlite3.connect(db_path)
+    print('filesdb created')
+    cursor = connection.cursor()
 
-# os.walk will go through each directory and collect all files
-for root, dirs, files in os.walk(path):
-    # root will get the path to the files
-    print('entered loop')
-    for file in files:
-        if file.split('.')[0] == '_' or file.split('.')[0] == '':
-            break
-        if len(file.split('.')) > 1:
-            fname = file
-            print(fname)
-            ext = file.split('.')[1]
-            path = root
-            cursor.execute('INSERT INTO files VALUES (?,?,?)', (ext, path, fname))
-        else:
-            fname = file
-            ext = None
-            path = root
-            cursor.execute('INSERT INTO files VALUES (?,?,?)', (ext, path, fname))
+    cursor.execute('''CREATE TABLE files (
+                ext TEXT,
+                path TEXT,
+                fname TEXT
+                )''')
 
-# Get all data from database
-cursor.execute('SELECT * FROM files')
-rows = cursor.fetchall()
+    # os.walk will go through each directory and collect all files
+    for root, dirs, files in os.walk(path):
+        # root will get the path to the files
+        print('entered loop')
+        for file in files:
+            if file.split('.')[0] == '_' or file.split('.')[0] == '':
+                break
+            if len(file.split('.')) > 1:
+                fname = file
+                print(fname)
+                ext = file.split('.')[1]
+                path = root
+                cursor.execute('INSERT INTO files VALUES (?,?,?)', (ext, path, fname))
+            else:
+                fname = file
+                ext = None
+                path = root
+                cursor.execute('INSERT INTO files VALUES (?,?,?)', (ext, path, fname))
 
-if os.path.exists(py_path + 'files-part1.txt'):
-    os.remove(py_path + 'files-part1.txt')
+    # Get all data from database
+    cursor.execute('SELECT * FROM files')
+    rows = cursor.fetchall()
 
-# Print query to .txt file
-with open(py_path + 'files-part1.txt', 'a+') as f:
-    for row in rows:
-        f.write(str(row) + '\n')
+    if os.path.exists(py_path + 'files-part1.txt'):
+        os.remove(py_path + 'files-part1.txt')
+
+    # Print query to .txt file
+    with open(py_path + 'files-part1.txt', 'a+') as f:
+        for row in rows:
+            f.write(str(row) + '\n')
 
 
-connection.commit()
+    connection.commit()
 
-connection.close()
+    connection.close()
+
+if __name__ == '__main__':
+    main()

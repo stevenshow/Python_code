bac4287a9201caf1a9f1d7501d871556171f5f63
Steven <steven.schoebinger@gmail.com>
2021-07-16 11:57:23 -0600

    formatted

--- practice/Website/blog_attempt/blog/views.py
+++ practice/Website/blog_attempt/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
     return HttpResponse('<h1>Home page</h1>')
+
 
 def about(request):
     return HttpResponse('<h1>About page</h1>')

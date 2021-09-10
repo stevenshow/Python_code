23b02da575958910a202c460ed04bd1c904c19b8
Steven Schoebinger <steven.schoebinger@gmail.com>
2021-09-10 02:42:32 -0600

    .

--- practice/GUI/PyQt5/Health_Tracker/h_tracker.py
+++ practice/GUI/PyQt5/Health_Tracker/h_tracker.py
@@ -8,7 +8,79 @@ import sqlite3
 
 class Ui_SecondWindow(object):
     def __init__(self):
-        pass
+        self.path = os.path.dirname(__file__) + '/'
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.instruction = QtWidgets.QLabel(self.centralwidget)
+        self.instruction.setGeometry(QtCore.QRect(200, 60, 300, 20))
+        self.instruction.setObjectName("instruction")
+        self.instruction.setText('Enter in your stats below then press save')
+
+        self.name_input = QtWidgets.QLineEdit(self.centralwidget)
+        self.name_input.setGeometry(QtCore.QRect(290, 120, 131, 28))
+        self.name_input.setObjectName("name_input")
+        self.name_label = QtWidgets.QLabel(self.centralwidget)
+        self.name_label.setGeometry(QtCore.QRect(220, 124, 58, 16))
+        self.name_label.setObjectName("name_label")
+
+        self.weight_input = QtWidgets.QLineEdit(self.centralwidget)
+        self.weight_input.setGeometry(QtCore.QRect(290, 162, 131, 28))
+        self.weight_input.setObjectName("weight_input")
+        self.weight_label = QtWidgets.QLabel(self.centralwidget)
+        self.weight_label.setGeometry(QtCore.QRect(220, 160, 51, 31))
+        self.weight_label.setObjectName("weight_label")
+
+        self.calorie_input = QtWidgets.QLineEdit(self.centralwidget)
+        self.calorie_input.setGeometry(QtCore.QRect(290, 202, 131, 28))
+        self.calorie_input.setObjectName("calorie_input")
+        self.calorie_label = QtWidgets.QLabel(self.centralwidget)
+        self.calorie_label.setGeometry(QtCore.QRect(219, 202, 61, 27))
+        self.calorie_label.setObjectName("calorie_label")
+
+        self.date_input = QtWidgets.QDateEdit(self.centralwidget)
+        self.date_input.setGeometry(QtCore.QRect(290, 242, 131, 24))
+        self.date_input.setObjectName("date_input")
+        self.date_label = QtWidgets.QLabel(self.centralwidget)
+        self.date_label.setGeometry(QtCore.QRect(220, 247, 48, 16))
+        self.date_label.setObjectName("date_label")
+
+        self.Save_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Save_button.setGeometry(QtCore.QRect(530, 400, 85, 28))
+        self.Save_button.setObjectName("Save_button")
+        self.Save_button.clicked.connect(lambda: MainWindow.close())
+        self.Exit_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Exit_button.setGeometry(QtCore.QRect(440, 400, 85, 28))
+        self.Exit_button.setObjectName("Exit_button")
+        self.Exit_button.clicked.connect(lambda:MainWindow.close())
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Health Tracker"))
+        self.Save_button.setText(_translate("MainWindow", "Load"))
+        self.Exit_button.setText(_translate("MainWindow", "Exit"))
+        self.date_label.setText(_translate("MainWindow", "Date"))
+        self.calorie_label.setText(_translate("MainWindow", "Calories"))
+        self.weight_label.setText(_translate("MainWindow", "Weight"))
+        self.name_label.setText(_translate("MainWindow", "Name"))
+    
+
+        
 
 
 class Ui_MainWindow(object):
@@ -74,16 +146,69 @@ class Ui_MainWindow(object):
         self.Save_button.setObjectName("Save_button")
         self.Save_button.clicked.connect(self.save_data)
         self.Exit_button = QtWidgets.QPushButton(self.centralwidget)
-        self.Exit_button.setGeometry(QtCore.QRect(440, 400, 85, 28))
+        self.Exit_button.setGeometry(QtCore.QRect(350, 400, 85, 28))
         self.Exit_button.setObjectName("Exit_button")
         self.Exit_button.clicked.connect(lambda:MainWindow.close())
+        self.Load_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Load_button.setGeometry(QtCore.QRect(440, 400, 85, 28))
+        self.Load_button.setObjectName("Load_button")
+        self.Load_button.clicked.connect(self.open_file_window)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Health Tracker"))
+        self.Load_button.setText(_translate("Load_button", "Load"))
         self.Save_button.setText(_translate("MainWindow", "Save"))
+        self.Exit_button.setText(_translate("MainWindow", "Exit"))
+        self.date_label.setText(_translate("MainWindow", "Date"))
+        self.calorie_label.setText(_translate("MainWindow", "Calories"))
+        self.weight_label.setText(_translate("MainWindow", "Weight"))
+        self.name_label.setText(_translate("MainWindow", "Name"))
+
+    def setupUi2(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.instruction = QtWidgets.QLabel(self.centralwidget)
+        self.instruction.setGeometry(QtCore.QRect(200, 60, 300, 20))
+        self.instruction.setObjectName("instruction")
+        self.instruction.setText('Enter in your stats below then press save')
+
+        self.name_input = QtWidgets.QLineEdit(self.centralwidget)
+        self.name_input.setGeometry(QtCore.QRect(290, 120, 85, 25))
+        self.name_input.setObjectName("file_input")
+        self.name_label = QtWidgets.QLabel(self.centralwidget)
+        self.name_label.setGeometry(QtCore.QRect(220, 124, 58, 16))
+        self.name_label.setObjectName("file_label")
+
+        self.Load_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Load_button.setGeometry(QtCore.QRect(530, 400, 85, 28))
+        self.Load_button.setObjectName("Load_button")
+        self.Load_button.clicked.connect(lambda: MainWindow.close())
+        self.Exit_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Exit_button.setGeometry(QtCore.QRect(440, 400, 85, 28))
+        self.Exit_button.setObjectName("Exit_button")
+        self.Exit_button.clicked.connect(lambda:MainWindow.close())
+        self.retranslateUi2(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi2(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Health Tracker"))
+        self.Load_button.setText(_translate("MainWindow", "Load"))
         self.Exit_button.setText(_translate("MainWindow", "Exit"))
         self.date_label.setText(_translate("MainWindow", "Date"))
         self.calorie_label.setText(_translate("MainWindow", "Calories"))
@@ -138,8 +263,9 @@ class Ui_MainWindow(object):
             self.connection.commit()
             self.connection.close()
 
-    def open_file(self):
-        pass
+    def open_file_window(self):
+        ui.setupUi2(MainWindow)
+        MainWindow.show()
 
 if __name__ == "__main__":
     import sys
